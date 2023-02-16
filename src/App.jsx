3282d6a1b5e529610a477e56db0a3fca80f808d4
Adithyavarun-creator <74570395+Adithyavarun-creator@users.noreply.github.com>
2023-02-16 14:56:43 +0530

    package updated

--- src/App.jsx
+++ src/App.jsx
@@ -52,3 +52,5 @@ const App = () => {
 };
 
 export default App;
+
+
---
+++ src/git.js
@@ -0,0 +1,11 @@
+//first initialize git - git init
+//then pull the file - git pull branch link
+//to create a branch - git branch
+//to enter branch name = git checkout -n "branch name"
+//to know status of new branch = git status
+//to add all updated files - git add .
+//to commit all changes - git commit -m "whatever changes done"
+//adding the branch to git link - git remote add origin "gitlink"
+//pull the old code and update with new branch - git pull origin main
+//check status if updated branch -  git status
+//push the updates to newly created branch - git push origin "branch name newly created"
